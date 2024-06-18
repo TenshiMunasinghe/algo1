ae6a9d6c33d5a50a9bb17eb0c2d16d52ef90d974
TenshiMunasinghe <shadowghost1007@gmail.com>
2024-06-18 18:54:03 +0900

    wip

--- le03/B.c
+++ le03/B.c
@@ -1,36 +1,89 @@
-#include
-#include
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #define LEN 100005
 
-typedef struct pp
+typedef struct
 {
   char name[100];
   int t;
 } P;
 
 P Q[LEN + 1];
-int head, tail, n;
+int head = 0, tail = 0, n;
+
+int isEmpty()
+{
+  return head == tail ? 1 : 0;
+}
+
+int isFull()
+{
+  return (head == (tail + 1) % n) ? 1 : 0;
+}
 
 void enqueue(P x)
 {
+  if (isFull() == 1)
+  {
+    printf("Memory overflow\n");
+    exit(1);
+  }
+  tail = (tail + 1) % n;
+  Q[tail] = x;
 }
 
 P dequeue()
 {
+  if (isEmpty() == 1)
+  {
+    printf("Memory underflow\n");
+    exit(1);
+  }
+  P x = Q[head];
+  // printf("head %d\n", head);
+  head = (head + 1) % n;
+
+  return x;
 }
 
 int main()
 {
-  int elaps = 0, c;
+  int elapse = 0;
   int i, q;
   P u;
+
   scanf("%d %d", &n, &q);
 
-  for (i = 1; i <= n; i++)
+  for (i = 0; i < n; i++)
   {
     scanf("%s", Q[i].name);
     scanf("%d", &Q[i].t);
+    // printf("insert %s %d\n", Q[i].name, Q[i].t);
   }
+  tail = n - 1;
+  // printf("initial %d %d\n", head, tail);
 
-  return 0;
+  while (isEmpty() == 0)
+  {
+    u = dequeue();
+    // printf("dequeue %d %d\n", head, tail);
+    // printf("u %s %d\n", u.name, u.t);
+    if (u.t > q)
+    {
+      elapse += q;
+      u.t -= q;
+      enqueue(u);
+      // printf("enqueue %d %d\n", head, tail);
+    }
+    else
+    {
+      elapse += u.t;
+      printf("%s %d\n", u.name, elapse);
+    }
+  };
+  elapse += Q[head].t;
+  printf("%s %d\n", Q[head].name, elapse);
+
+  return EXIT_SUCCESS;
 }
