359fb52a6cae1e435e716d611c937770b58573ba
TenshiMunasinghe <shadowghost1007@gmail.com>
2024-07-09 12:03:24 +0900

    wip

---
+++ le09/A.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+
+  int heap[n];
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &heap[i]);
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    int node, key, parent, left, right;
+    node = i + 1;
+    key = heap[i];
+
+    printf("node %d: key = %d", node, key);
+
+    if ((node / 2) - 1 != node)
+    {
+      parent = heap[(node / 2) - 1];
+      printf(" parent key = %d,", parent);
+    }
+
+    if (node * 2 - 1 < n)
+    {
+      left = heap[node * 2 - 1];
+      printf(" left key = %d,", left);
+    }
+    if ((node * 2) < n)
+    {
+      right = heap[node * 2];
+      printf(" right key = %d,", right);
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ le09/B.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void maxHeapify(int n, int *heap, int idx)
+{
+  int largest, l, r;
+  l = (idx + 1) * 2 - 1;
+  r = (idx + 1) * 2;
+
+  largest = (l < n && heap[l] > heap[idx]) ? l : idx;
+  largest = (r < n && heap[r] > heap[largest]) ? r : largest;
+
+  if (largest != idx)
+  {
+    int temp = heap[idx];
+    heap[idx] = heap[largest];
+    heap[largest] = temp;
+    maxHeapify(n, heap, largest);
+  }
+}
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+
+  int heap[n];
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d", &heap[i]);
+  }
+
+  for (int i = n / 2; i >= 0; i--)
+    maxHeapify(n, heap, i);
+
+  for (int i = 0; i < n - 1; i++)
+  {
+    printf("%d ", heap[i]);
+  }
+  printf("%d\n", heap[n - 1]);
+
+  return 0;
+}
